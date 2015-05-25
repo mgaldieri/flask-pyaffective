56153953f67c2f00d794fc73a1f07084706fac39
mgaldieri <mgaldieri@gmail.com>
2015-05-25 15:33:27 -0300

    Actuator class.

--- general_tests.py
+++ general_tests.py
@@ -1,23 +1,47 @@
 __author__ = 'mgaldieri'
 
-from pyaffective.agent import Agent
-from time import sleep
+from scipy.spatial.distance import cosine
+from scipy.spatial import distance
+import numpy as np
 
-agent = Agent()
-agent.start()
-
-for i in range(5):
-    sleep(1)
-    print i+1
-
-agent.stop()
-
-for i in range(2):
-    sleep(1)
-    print i+1
+# agent = Agent()
+# agent.start()
+#
+# for i in range(5):
+#     sleep(1)
+#     print i+1
+#
+# agent.stop()
+#
+# for i in range(2):
+#     sleep(1)
+#     print i+1
 
 # def test(*args):
 #     # args = args.append(3)
 #     print args
 #
 # test(1, 2)
+
+features = [np.array([0.1, 0.3, 0.2]),
+            np.array([0.3, 0.7, 0.6]),
+            np.array([0.2, 0.4, 0.7]),
+            np.array([-0.4, -0.7, -0.9]),
+            np.array([0.3, 0.6, 0.1])]
+
+b = np.array([0.2, 0.4, 0.7])
+
+
+# print np.array(c)/np.linalg.norm(c, 1)
+
+# print max(enumerate([1-cosine(x, b) for x in a]))
+
+scores = np.array([distance.sqeuclidean(x, b) for x in features])
+print 1-(scores/np.linalg.norm(scores, 1))
+#
+# print np.array([1, 1, 1, 1]) * np.array([1, 2, 3, 4])
+
+# rgbs = np.array([np.array([0.3, 0.5, 0.7]), np.array([0.1, 0.7, 0.7]), np.array([0.9, 0.3, 0.2]), np.array([0.2, 0.9, 0.1])])
+# weights = np.array([0.0, 1.0, 0.0, 0.0])
+#
+# print np.average(rgbs, axis=0, weights=weights)
--- utils/actuators.py
+++ utils/actuators.py
@@ -1,1 +1,42 @@
 __author__ = 'mgaldieri'
+
+from pyaffective.emotions import PAD
+from scipy.spatial.distance import sqeuclidean
+from operator import itemgetter
+import numpy as np
+
+
+class Feature:
+    def __init__(self, name='', pad=PAD(), rgb=None):
+        if not rgb:
+            rgb = []
+        self.name = name
+        self.pad = pad
+        self.rgb = np.array(rgb)
+
+
+class RGBled:
+    def __init__(self, features=[Feature()]*5):
+        self.features = features if all([isinstance(f, Feature) for f in features]) else [Feature()]*5
+        self.f_vec = [f.pad.state for f in self.features]
+        self.rgb_vec = [f.rgb for f in self.features]
+
+    def rgb(self, value=PAD()):
+        """
+        RGB values for discrete features
+        :param value: mood value wrapped in a PAD object
+        :return: numpy array containing r, g and b values between 0 and 1
+        """
+        similarities = np.array([1-sqeuclidean(f, value.state) for f in self.f_vec])
+        strongest = max(enumerate(1-similarities/np.linalg.norm(similarities, 1)), key=itemgetter(1))
+        return self.rgb_vec[strongest[0]]
+
+    def rgb_alt(self, value=PAD()):
+        """
+        Averaged RGB values between all measured features
+        :param value: mood value wrapped in a PAD object
+        :return: numpy array containing r, g and b values between 0 and 1
+        """
+        similarities = np.array([1-sqeuclidean(f, value.state) for f in self.f_vec])
+        weights = 1-similarities/np.linalg.norm(similarities, 1)
+        return np.average(self.rgb_vec, axis=0, weights=weights)
